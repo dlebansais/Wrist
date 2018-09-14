ad0d396d38718972472ad3d2c017d7286134a9c7
dlebansais <dlebansais@gmail.com>
2018-09-14 19:20:31 +0200

    Guid string format fix.

--- NetTools/SecretUuid.cs
+++ NetTools/SecretUuid.cs
@@ -6,8 +6,9 @@
         {
 #error Update GuidBytes declaration with your own guid, then remove this error line.
             GuidBytes = new byte[16] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-            string GuidString = "{" + HashTools.GetString(GuidBytes) + "}";
-            Guid = new System.Guid(GuidString);
+            string GuidString = HashTools.GetString(GuidBytes);
+            GuidString = GuidString.Substring(0, 8) + "-" + GuidString.Substring(8, 4) + "-" + GuidString.Substring(12, 4) + "-" + GuidString.Substring(16, 4) + "-" + GuidString.Substring(20, 12);
+            Guid = new System.Guid("{" + GuidString + "}");
         }
 
         public static byte[] GuidBytes { get; private set; }
