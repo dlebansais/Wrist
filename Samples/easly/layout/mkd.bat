dc3040057c5d26704baeef39088038178c1bb885
David Le Bansais <dlebansais@gmail.com>
2018-10-26 15:17:15 +0200

    Deleted unused files.

--- Samples/easly/layout/mkd.bat
+++
@@ -1,2 +0,0 @@
-mkdir "feature %1"
-copy "feature abstract methods\feature abstract methods.xaml" "feature %1\feature %1.xaml"
--- Samples/easly/layout/mkd2.bat
+++
@@ -1,2 +0,0 @@
-mkdir "feature %1 %2"
-copy "feature abstract methods\feature abstract methods.xaml" "feature %1 %2\feature %1 %2.xaml"
