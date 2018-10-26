7ef6b6b3656c77c496e5250e3452f2768d4e4f01
David Le Bansais <dlebansais@gmail.com>
2018-10-26 15:16:38 +0200

    Misc changes.

---
+++ Samples/easly/layout/mkd.bat
@@ -0,0 +1,2 @@
+mkdir "feature %1"
+copy "feature abstract methods\feature abstract methods.xaml" "feature %1\feature %1.xaml"
---
+++ Samples/easly/layout/mkd2.bat
@@ -0,0 +1,2 @@
+mkdir "feature %1 %2"
+copy "feature abstract methods\feature abstract methods.xaml" "feature %1 %2\feature %1 %2.xaml"
